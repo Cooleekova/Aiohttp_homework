0e4801b2d6b3b004d884612751949e62a5d1105f
Atya <nushnushka@gmail.com>
2021-07-29 17:17:24 +0300

    routes added to app

--- app/server.py
+++ app/server.py
@@ -24,7 +24,7 @@ class BaseModel:
         instance = await cls.get(obj_id)
         if instance:
             return instance
-        raise web.HTTPNotFound()
+        raise web.HTTPNotFound(text='данного ID нет в базе')
 
     @classmethod
     async def create_instance(cls, **kwargs):
@@ -33,13 +33,6 @@ class BaseModel:
         except UniqueViolationError:
             raise web.HTTPBadRequest()
         return instance
-
-    @classmethod
-    async def delete(cls, obj_id):
-        instance = await cls.delete(obj_id)
-        if instance:
-            return instance
-        raise web.HTTPNotFound
 
 
 class User(db.Model, BaseModel):
@@ -79,19 +72,27 @@ class Ad(db.Model, BaseModel):
     __tablename__ = 'advertisements'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), index=True, nullable=False)
-    description = db.Column(db.Text, index=True, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
     creator_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    _idx1 = db.Index('advertisements_ad_title', 'title', unique=True)
 
     def to_dict(self):
         return {
             'id': self.id,
             'title': self.title,
             'description': self.description,
-            'created_at': self.created_at,
+            'created_at': str(self.created_at),
+            'creator_id': self.creator_id,
         }
+
+    def __str__(self):
+        return '<Advertisment {}>'.format(self.title)
+
+    def __repr__(self):
+        return str(self)
 
 
 async def set_connection():
@@ -119,13 +120,11 @@ async def orm_engine(app):
 
 class UserView(web.View):
 
-    @routes.get('/users/<int:user_id>')
     async def get(self):
         user_id = int(self.request.match_info['user_id'])
         user = await User.get_or_404(user_id)
         return web.json_response(user.to_dict())
 
-    @routes.post('/users')
     async def post(self):
         data = await self.request.json()
         user = await User.create_instance(**data)
@@ -134,27 +133,21 @@ class UserView(web.View):
 
 class AdView(web.View):
 
-    @routes.get('/ads/hello')
-    async def hello(self):
-        return web.Response(text=f'HELLO')
-
-    @routes.get('/ads/<int:ad_id>')
-    async def get_ad(self):
+    async def get(self):
         ad_id = int(self.request.match_info['ad_id'])
         ad = await Ad.get_or_404(ad_id)
         return web.json_response(ad.to_dict())
 
-    @routes.post('/ads')
-    async def post_ad(self):
+    async def post(self):
         data = await self.request.json()
         ad = await Ad.create_instance(**data)
         return web.json_response(ad.to_dict())
 
-    @routes.delete('/ads/<int:ad_id>')
-    async def delete_ad(self):
+    async def delete(self):
         ad_id = int(self.request.match_info['ad_id'])
-        await Ad.delete(ad_id)
-        return web.Response(text=f'Ad was deleted')
+        instance = await Ad.get_or_404(ad_id)
+        await instance.delete()
+        return web.Response(text=f'Ad {instance} was deleted')
 
 
 @routes.get('/health')
@@ -162,15 +155,21 @@ async def health(request):
     pool = request.app['pg_pool']
     async with pool.acquire() as conn:
         async with conn.cursor() as cursor:
-            await cursor.execute('SELECT * from star_war_persons')
+            await cursor.execute('SELECT 1')
             data = await cursor.fetchall()
-
             return web.Response(body=json.dumps({'data': data}))
 
 
 app.cleanup_ctx.append(orm_engine)
 app.cleanup_ctx.append(pg_pool)
 
-app.add_routes(routes)
+app.add_routes([
+    web.get('/users/{user_id:\d+}', UserView),
+    web.post('/users', UserView),
+    web.get('/ads/{ad_id:\d+}', AdView),
+    web.post('/ads', AdView),
+    web.delete('/ads/{ad_id:\d+}', AdView),
+     ])
+
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 web.run_app(app, port=8088)
